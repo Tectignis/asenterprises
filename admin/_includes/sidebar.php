b2215f500a99307771289c14d2dfdbda931c8452
snehalambavale <snehal7039@gmail.com>
2022-07-20 13:50:50 +0530

    title

--- admin/_includes/sidebar.php
+++ admin/_includes/sidebar.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
-  <a href="../../index3.html" class="brand-link">
+  <a href="index.php" class="brand-link">
     <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">AdminLTE 3</span>
   </a>
@@ -14,7 +14,7 @@
       </div>
       <div class="info">
   
-        <a href="#" class="d-block">ASENTERPRISES</a>
+        <a href="index.php" class="d-block">ASENTERPRISES</a>
       </div>
     </div>
 
@@ -30,8 +30,7 @@
             <i class="nav-icon fas fa-th"></i>
            
             <p>
-              Home
-             
+              Home 
             </p>
           </a>
         </li>
@@ -83,7 +82,7 @@
         </li>   
       <li class="nav-item">
         <a href="testimonials.php" class="nav-link">
-        <i class="fa-solid fa-people-pants-simple"></i>
+        <i class="nav-icon fab fa-accessible-icon"></i>
             <p>
             Testimonials
           </p>
@@ -93,6 +92,7 @@
         <li class="nav-item">
           <a href="changepassword.php" class="nav-link">
             <!-- <i class=" nav-icon far fa-solid fa-handshake"></i> -->
+            <i class="nav-icon fas fa-solid fa-key"></i>
             <p>
               Change password
             </p>
--- admin/files/contactus.php
+++ admin/files/contactus.php
@@ -19,7 +19,7 @@ if(!isset($_SESSION['id'])){
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | DataTables</title>
+  <title>AS | CONTACT</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet"
